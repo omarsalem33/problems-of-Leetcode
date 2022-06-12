b79b9d6330dafc8e1ed8c61c45451a3e1a132332
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2022-06-12 04:20:30 +0200

    Time: 337 ms (77.77%), Space: 99.5 MB (89.43%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        
+           unordered_map<int, int>m;
+            int sum = 0, i = 0, j = 0, Max = 0;
+        while(j < nums.size()){
+            sum += nums[j];
+            m[nums[j]] ++;
+            if(m[nums[j]] > 1){
+                while(m[nums[j]] != 1){
+                    m[nums[i]]--;
+                    sum -= nums[i];
+                    i++;
+                }
+            }
+            j++;
+            Max = max(Max, sum);
+        }
+        return Max;
+            
+    }
+};
