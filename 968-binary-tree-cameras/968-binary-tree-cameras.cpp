c4d006f88e91bb80c118a9755fa362047b5089af
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2022-06-17 09:51:26 +0200

    Time: 15 ms (64.53%), Space: 21.3 MB (55.06%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = 0;
+    int minCameraCover(TreeNode* root) 
+    {
+        return (dfs(root) < 1 ? 1 : 0) + res;
+    }
+
+    int dfs(TreeNode* root) 
+    {
+        if (!root) 
+                return 2;
+        int left = dfs(root->left), right = dfs(root->right);
+        if (left == 0 or right == 0) 
+        {
+            res++;
+            return 1;
+        }
+        return left == 1 or right == 1 ? 2 : 0;
+    }
+};
