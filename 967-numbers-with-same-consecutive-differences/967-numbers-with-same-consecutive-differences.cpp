38ece39047a485a48ddc1970006ac856a8badeda
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2022-09-03 11:55:30 +0200

    Time: 0 ms (100.00%), Space: 7.6 MB (70.71%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,19 +1,26 @@
 class Solution {
-public:
-  vector<int> numsSameConsecDiff(int n, int k) {
-        deque<int> q{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        while (--n ) {
-            int sz = q.size();
-            for (int i = 0; i < sz; i++) {
-                int p = q.front();
-                q.pop_front();
-                for (int j = 0; j < 10; j++) {
-                  if (abs((p % 10) - j) == k) {
-                    q.push_back(p * 10 + j);
-                  }
+        vector < int > ans ;
+        void Solve(int len , int &k , int num , int digit)
+        {
+                if(len == 0 )
+                {
+                        ans.emplace_back(num);
+                        return ;
                 }
-          }
+                for(int i = 0 ; i < 10 ; i++)
+                {
+                       if(abs(i - digit) == k)
+                       {
+                               Solve(len-1 , k , num * 10 + i , i);
+                       }
+                }
         }
-        return vector<int>{q.begin(), q.end()};
+        
+public:
+    vector<int> numsSameConsecDiff(int n, int k)
+    {
+        for(int i = 1 ; i< 10 ; i++)
+             Solve(n-1 , k, i, i);
+        return ans;
     }
 };
