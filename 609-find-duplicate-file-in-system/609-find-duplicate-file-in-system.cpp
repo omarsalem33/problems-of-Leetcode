d01a18e76dbe83dd923ed8ddc380f24b342988db
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2022-09-19 09:46:14 +0200

    Time: 183 ms (55.34%), Space: 38.9 MB (58.40%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,47 +1,35 @@
 class Solution {
 public:
     vector<vector<string>> findDuplicate(vector<string>& paths) {
-        unordered_map<string, vector<string>> mp;
-        for(auto str : paths)
+
+        map < string , vector < string > > mp;
+        for(auto & s : paths)
         {
-            int n = str.size();
+            int n = s.size();
             int i = 0;
             string path = "";
-            while(i < n and str[i] != ' ')
-            {
-                path += str[i];
-                i++;
-            }
+            while(i < n and s[i] != ' ')
+                path += s[i] , i++;
             path += '/';
             i++;
-            while(i < n)
+            while (i < n)
             {
                 string file_name = "";
                 string file_ext = "";
-                while(i < n and str[i] != '(')
-                {
-                    file_ext += str[i];
-                    i++;
-                }
-                while(i < n and str[i] != ' ')
-                {
-                    file_name += str[i];
-                    i++;
-                }
-                mp[file_name].push_back(path + file_ext);
-                i++;
+                while (i < n and s[i] != '(')
+                    file_ext += s[i] ,i++;
+                while (i < n and s[i] != ' ')
+                    file_name += s[i] ,i++;
+                mp [file_name].push_back(path + file_ext) ,i++;
             }
         }
-        
-        vector<vector<string>> res;
+        vector<vector<string>> ans;
         for(auto x : mp)
-        {
             if(x.second.size() > 1)
-            {
-                res.push_back(x.second);
-            }
-        }
+                ans.push_back(x.second);
         
-        return res;
+        return ans;
     }
 };
+
+
