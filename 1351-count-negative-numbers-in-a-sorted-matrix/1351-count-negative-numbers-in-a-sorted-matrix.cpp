bcda815486646993925cb6f6f727a51291e81f1e
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2022-10-16 06:10:02 +0200

    Time: 48 ms (9.07%), Space: 10.4 MB (45.32%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -3,12 +3,8 @@ public:
     int countNegatives(vector<vector<int>>& grid) {
         int ans = 0;
         for(auto &i : grid)
-        {
-            for(auto &j : i)
-            {
-                if(j < 0)
-                    ans++;
-            }
+        {   
+            ans += upper_bound(i.rbegin() , i.rend() , -1) - i.rbegin();
         }
     return ans ;
     }
