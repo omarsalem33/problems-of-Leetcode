a990b80782532e807d51f52e87894666b73ba377
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2022-10-17 07:09:52 +0200

    Time: 11 ms (9.70%), Space: 6.7 MB (31.31%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        if(sentence.size() < 26)
-            return false;
-        map < char , int >mp ;
-        for(char i = 'a'; i <= 'z' ; i++)
-            mp[i]++;
-        for(char &i :sentence)
-            mp[i]++;
-        for(auto &i : mp)
-            if(i.second <2)
-                return false;
-        return true;
+        set<char> st (sentence.begin(), sentence.end());         
+        return st.size()==26;
     }
 };
