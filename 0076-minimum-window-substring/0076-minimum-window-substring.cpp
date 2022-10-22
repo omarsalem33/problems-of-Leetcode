417db4c3b405869c09ff3803fadc34d7d19aaecb
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2022-10-22 20:53:18 +0200

    Time: 561 ms (5.01%), Space: 8.2 MB (31.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        unordered_map<char, int> m;
-        for(char &c : t)
-            m[c]++;
-
-        int i = 0, d = INT_MAX, cnt = m.size();
-        string ans;
-        for(int j=0; j<s.size(); j++) {
-            if(m.find(s[j]) != m.end()) {
-                m[s[j]]--;
-                if(!m[s[j]]) cnt--;
+    bool check(vector < int >& freq_s, vector < int >& freq_t){
+        for(int i = 0; i < 256; i++)
+            if(freq_t[i] > freq_s[i]) 
+                return false;
+        return true;
+    }
+    
+    string minWindow(string& s, string& t) {
+        vector < int > freq_s(256), freq_t(256);
+        for(auto& c : t)
+            freq_t[c - 'A']++;
+       
+        int l = 0, r = 0, n = s.size(), Min = INT_MAX;
+        string window;
+        while(l < n){
+            while(r < n && !check(freq_s, freq_t)) 
+                freq_s[s[r] - 'A']++, r++;
+            if(Min > r - l && check(freq_s, freq_t))
+            {
+                Min = r - l;
+                window.clear();
+                for(int i = l; i < r; i++)
+                    window += s[i];
             }
-            while(!cnt) {
-                if(j - i + 1 < d) 
-                    d = j - i + 1, ans = s.substr(i, j - i + 1);
-                if(m.find(s[i]) != m.end()) 
-                {
-                    m[s[i]]++;
-                    if(m[s[i]] > 0)
-                        cnt++;
-                }
-                i++;
-            }
+            freq_s[s[l] - 'A']--, l++;
         }
-        return ans;
-    } 
+        return window;
+        
+    }
 };
+
