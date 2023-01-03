5cd9431a62fb5c301bb6ca434dd7fc761822e950
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2023-01-03 06:56:32 +0200

    Update 0944-delete-columns-to-make-sorted.cpp

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+// Author : Omar Salem
  int minDeletionSize(vector<string>& strs) {
         int ans =0;
         for(int i=0;i<strs[0].size();i++){
             for(int j=1;j<strs.size();j++){
+// if previous row as higher value mean not sorted hence increase the value of ans and break the inner loop for checking next column.
                 if(strs[j-1][i]>strs[j][i]){
                     ans++;
                     break;
@@ -12,6 +14,5 @@ public:
         }
 
         return ans;
-
     }
 };
