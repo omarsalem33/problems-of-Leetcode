e3d6231504352b5b274b9c9614bf659868822d0b
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2025-07-05 18:27:58 +0300

    Time: 2 ms (55.31%), Space: 8 MB (4.46%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -13,11 +13,11 @@ public:
         int l = 0 , r = n;
         while(l <= r)
         {
-             int mid =l +(r- l) / 2; 
+            int mid  = l + (r-l) / 2;
             int num = guess(mid);
-            if(num == 0)
+            if(num == 0 )
                 return mid;
-            (num > 0 ? l = mid + 1 : r= mid - 1);
+            (num >  0 ? l = mid +1 : r= mid -1);
         }
         return -1;
     }
