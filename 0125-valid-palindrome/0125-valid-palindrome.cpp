ba954fd7c729a778980f0c5dbeae519d02ca9f4e
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2025-07-20 17:47:27 +0300

    Time: 4 ms (17.08%), Space: 10.3 MB (28.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if (s.size() == 0 or s.size() == 1)
+        if(s.size() <= 1)
             return true;
-
         string ans = "";
-        for (auto& c : s) {
-            c = tolower(c);
-            if (isalpha(c) || isdigit(c))
-                ans.push_back(c);
-        }
-        int l = 0, r = ans.size() - 1;
-        while (l <= r) {
-            if (ans[l] == ans[r])
+        for(auto &i : s){
+            i = tolower(i);
+            if(isalpha(i) or isdigit(i))
+                ans.push_back(i);
+            }
+        int l = 0 , r =ans.size()-1;
+        while(l <= r){
+            if(ans[l] == ans[r])
                 l++, r--;
             else
-                return false;
+                return false ;
         }
+        
         return true;
+        
     }
+    
 };
